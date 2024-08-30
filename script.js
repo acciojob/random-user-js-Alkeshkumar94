5e872e6edaf221f18fe55d4f3b2d13257cf11d32
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-08-30 22:28:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+async function fetchUser(){
+	try{
+	const response=await fetch('https://randomuser.me/api/');
+    const data=await response.json();
+	const user=data.results[0];
+		document.getElementById("userName").textContent=`${user.name.first} ${user.name.last}`
+	}
+}
